bcec9ca067f8a5b230e3baf5e66a79dc7a4dc009
Aamir Ali <aa882755@gmail.com>
2024-02-12 06:56:03 +0530

    Creating our nuget package

---
+++ xyz.math.cal/Package/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace xyz.math.cal;
+
+public class Calculator
+{
+    public int Add(int x, int y) => x + y;
+    public int Sub(int x, int y) => x - y;
+    public double Multiple(int x, int y) => (double)x * y;
+    public double Div(int x, int y) => (double)x / y;
+}
+//https://www.youtube.com/watch?v=AHXrBjQge5s
---
+++ xyz.math.cal/README.md
@@ -0,0 +1,7 @@
+# MyPackage
+This is Test Try
+MyPackage is a lightweight and easy-to-use package for performing common tasks in C#.
+
+## Installation
+
+You can install MyPackage via NuGet Package Manager Console by running the following command:
