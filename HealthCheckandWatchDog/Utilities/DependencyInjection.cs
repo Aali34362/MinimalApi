bb06e7e72496551b3a0ef8a617dad601a43d212c
Aamir Ali <aa882755@gmail.com>
2024-02-17 21:12:11 +0530

    Health Check & Watch Dog Implementation With internal Concept understanding documentation

---
+++ HealthCheckandWatchDog/Utilities/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthCheckandWatchDog.Utilities;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+}
