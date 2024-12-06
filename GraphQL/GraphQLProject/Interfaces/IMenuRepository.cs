3ad28b266616ae2c402b26b5d86d1373ed4bc0b7
Amir Abdul Wahid <aa882755@gmail.com>
2024-12-06 09:54:16 +0530

    commit

---
+++ GraphQL/GraphQLProject/Interfaces/IMenuRepository.cs
@@ -0,0 +1,12 @@
+﻿using GraphQLProject.Models;
+
+namespace GraphQLProject.Interfaces;
+
+public interface IMenuRepository
+{
+    List<Menu> GetAllMenu();
+    Menu GetMenuById(Guid id);
+    Menu AddMenu(Menu menu);
+    Menu UpdateMenu(Guid id, Menu menu);
+    void DeleteMenu(Guid id);
+}
---
+++ GraphQL/GraphQLProject/Services/MenuRepository.cs
@@ -0,0 +1,32 @@
+﻿using GraphQLProject.Interfaces;
+using GraphQLProject.Models;
+
+namespace GraphQLProject.Services;
+
+public class MenuRepository : IMenuRepository
+{
+    public List<Menu> GetAllMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Menu GetMenuById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Menu AddMenu(Menu menu)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteMenu(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Menu UpdateMenu(Guid id, Menu menu)
+    {
+        throw new NotImplementedException();
+    }
+}
