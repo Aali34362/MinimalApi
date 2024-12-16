4d54df1524d9292fb8f3dab188605b09e8a13536
Amir Abdul Wahid <aa882755@gmail.com>
2024-12-16 20:42:16 +0530

    commit

---
+++ GraphQL/GraphQLProject/MiddlewareServices/GraphQLMiddlewareExtensions.cs
@@ -0,0 +1,28 @@
+﻿using GraphQL;
+using GraphQL.Types;
+
+namespace GraphQLProject.MiddlewareServices;
+
+public class GraphQLMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext context, IDocumentExecuter executer, ISchema schema)
+    {
+        // Handle GraphQL Playground
+       
+
+        // Handle GraphQL endpoint
+       
+
+        // Pass to the next middleware if not handled
+        await _next(context);
+    }
+}
+public static class GraphQLMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCustomGraphQL(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<GraphQLMiddleware>();
+    }
+}
