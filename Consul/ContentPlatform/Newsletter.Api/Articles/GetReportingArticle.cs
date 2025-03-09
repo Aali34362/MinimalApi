e3d5ef4ab9c9894e9638b00823bbb691dbac5de1
Amir Abdul Wahid <aa882755@gmail.com>
2025-03-09 19:52:37 +0530

    Consul

---
+++ Consul/ContentPlatform/Newsletter.Api/Articles/GetReportingArticle.cs
@@ -0,0 +1,24 @@
+﻿namespace Newsletter.Api.Articles;
+
+public class GetReportingArticle
+{
+    public class Response
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+        public DateTime? PublishedOnUtc { get; set; }
+
+        public List<ArticleEventResponse> Events { get; set; } = new();
+    }
+
+    public class ArticleEventResponse
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+        public int EventType { get; set; }
+    }
+}
